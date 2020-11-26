0e8463ac7d58ea4ca4f4a0e83cec820a106363fa
Pablo <moronpablo94@gmail.com>
2020-11-26 00:45:37 -0300

    Avance general estilos y features

---
+++ mascotas_react/src/common/components/ImagePreview.tsx
@@ -0,0 +1,18 @@
+import React, { useRef } from 'react'
+
+interface ImagePreviewProps {
+    src: string,
+    alt:string,
+    height:string,
+}
+export default function ImagePreview(props: ImagePreviewProps) {
+
+    return (
+        <div>
+            <img src={props.src}
+                alt={props.alt}
+                height={props.height}
+            />
+        </div>
+    )
+}
--- mascotas_react/src/notices/Publisher.tsx
+++ mascotas_react/src/notices/Publisher.tsx
@@ -1,52 +1,118 @@
 import { Avatar } from '@material-ui/core'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Form from '../common/components/Form'
 import FormAcceptButton from '../common/components/FormAcceptButton'
-import ImageUpload from '../common/components/ImageUpload'
 import './Publisher.css'
 import './NoticeServices'
 import FindAvatar from './NoticeServices'
+import ImagePreview from '../common/components/ImagePreview'
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import VideocamIcon from '@material-ui/icons/Videocam';
 
 export default function Publisher() {
-    const [image, setImage] = useState('/assets/profile.png');
+    const [image, setImage] = useState('/assets/pig_nose.ico');
     const [message, setMessage] = useState('')
-    const [avatar, setAvatar]=useState('')
+    const [avatar, setAvatar] = useState('')
+    const fileInput = useRef<HTMLInputElement>(null)
 
-    const loadAvatar=async()=>{
-        try{
+    const loadAvatar = async () => {
+        try {
             const res = await FindAvatar();
             setAvatar(res);
-            console.log("ya seteo el avatar "+res)
-        }catch(error){
+            console.log("ya seteo el avatar " + res)
+        } catch (error) {
             console.log(error)
         }
     }
     useEffect(() => {
-       void loadAvatar();
+        void loadAvatar();
     }, [])
 
-    const PreviewImage=(image: string)=>{ //Viene un base64 desde el handler de ImageUpload
+    const PreviewImage = (image: string) => { //Viene un url desde el handler de ImagePreview
         setImage(image)
     }
 
-    const Publish=()=>{
 
+
+
+    const HandleSubmit = () => {
+        alert('Publishing!!' + message)
+
+        setMessage('');
+        setImage('/assets/pig_nose.ico');
+    }
+
+    const HandleLive = () => {
+        alert('live');
+    }
+
+    const imageSelect = () => {
+        const files = fileInput.current?.files
+        if (files == null) {
+            return
+        }
+        if((files)[0]==null){
+            setImage("/assets/pig_nose.ico");
+            return
+        }
+        
+        let url = URL.createObjectURL((files)[0]);
+        if (url) {
+            console.log(url)
+            setImage(url)
+        }
+
+    }
+    const HandlePhoto = () => {
+        fileInput.current?.click()
+    }
+
+    const HandleVideo = () => {
+        alert('VideoStory')
     }
 
     return (
         <div className="publisher">
             <div className="publisher__top">
-                <Avatar src={avatar}/>
-                <Form >
-                    <input 
-                    type="text"
-                    placeholder="Publica tu mascota!"
+                <Avatar src={avatar} />
+                <Form>
+                    <input
+                        className="publisher__input"
+                        type="text"
+                        placeholder="Muéstranos tu mascota :)"
+                        onChange={e => setMessage(e.target.value)}
+                        value={message}
                     />
-                    <ImageUpload src={image} onChange={PreviewImage}/>
-                    <FormAcceptButton label="Publicar" onClick={Publish} />
+                    <ImagePreview src={image} alt="50" height="50" />
+                    <div className="publisher__button">
+                        <button type="submit" onClick={HandleSubmit}>
+                            Publicar
+                    </button>
+                    </div>
                 </Form>
+            </div>
+
+            <div className="publisher__bottom">
+                <div className="publisher__option" onClick={HandlePhoto}>
+                    <PhotoLibraryIcon style={{ color: "green" }} />
+                    <input type="file"
+                        className="upload"
+                        accept="image/*"
+                        style={{ display: "none" }}
+                        ref={fileInput}
+                        onChange={imageSelect} />
+                    <h3>Foto</h3>
+                </div>
+                <div className="publisher__option" onClick={HandleVideo}>
+                    <VideoLibraryIcon style={{ color: "red" }} />
+                    <h3>Video</h3>
+                </div>
+                <div className="publisher__option" onClick={HandleLive}>
+                    <VideocamIcon style={{ color: "red" }} />
+                    <h3>Transimit en vivo</h3>
+                </div>
             </div>
         </div>
     )
-
 }
